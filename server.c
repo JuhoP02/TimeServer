fe206328fcc61d0dd6a8610c70596aee4697e2fa
JuhoP02 <125037081+JuhoP02@users.noreply.github.com>
2023-02-16 13:54:09 +0000

    Testing time difference

--- server.c
+++ server.c
@@ -46,14 +46,41 @@ int main(void) {
 
     // Wait for empty datagram
     recvfrom(sckt, buf, 0, 0, (struct sockaddr *)&channel, &addr_size);
+
+    struct tm start_time; // 00:00 Jan 1 1900
+    start_time.tm_hour = 0;
+    start_time.tm_sec = 0;
+    start_time.tm_mon = 0; // Jan
+    start_time.tm_yday = 0;
+    start_time.tm_mday = 1;
+
+     struct tm start_time_late; // 00:00 Jan 1 1900
+    start_time_late.tm_hour = 0;
+    start_time_late.tm_sec = 0;
+    start_time_late.tm_mon = 0; // Jan
+    start_time_late.tm_yday = 0;
+    start_time_late.tm_mday = 1;
+    start_time_late.tm_year = 70;
+
+    // convert to time_t
+    time_t t1 = mktime(&start_time);
+    time_t t2 = mktime(&start_time_late);
+
+    double diff_time = difftime(t2, t1);
+
+    time_t current_time = time(NULL); // Feb 16 2023 (time between (1900 + 70) years and 2023)
+
+    int32_t test = (int32_t)diff_time - (int32_t)current_time;
     
     // Get time in seconds and cast to int (4 bytes)
-    int32_t seconds = (int32_t)time(NULL) - (int32_t)2208988800;
-    time_t local = time(NULL);
-    printf("Connection received, seconds: %d, local time: %s\n", seconds, ctime(&local));
+    //int32_t seconds = (int32_t)time(NULL) - (int32_t)2208988800;
+    //time_t local = time(NULL);
+    //printf("Connection received, seconds: %d, local time: %s\n", seconds, ctime(&local));
+
+    int32_t packet = htonl(test);
     
     // Host bitorder to network
-    int32_t packet = htonl(seconds);
+    //int32_t packet = htonl(seconds);
   
     // Send time    
     sendto(sckt, &packet, sizeof(int32_t), 0, (struct sockaddr *)&channel, addr_size);
